4cd2dedacbbe57f20a0620c4a0dbf475ebb2d23a
kopytkoprog <196858@student.pwr.edu.pl>
2015-11-03 21:04:42 +0100

    Calculation created

--- Include/Config.h
+++ Include/Config.h
@@ -5,26 +5,26 @@
 // PD5 : OC1A
 // PD4 : OC1B
 
-
-
 //========== LCD ================
-#define LCD_FORMAT 				(" %4.1f %4.1f %4.1f")
+#define LCD_FORMAT 				("%5.2f %4.1f %4.1f")
 
 //========== MATH ===============
 // Internal Voltage Reference  min 2.3  typ 2.56  max 2.7
 #define VREF 					(2.51)
-#define SCALE_12V 				(1.0/3.0)
-#define SCALE_5V 				(4.0/5.0)
 
+#define SCALE_12V 				(1.0 / (1.0 + 4.7))
+#define SCALE_5V 				(1.0 / (1.0 + 1.0))
 
+#define SCALE_OPAMP				(3.3 / 2.2)
+
+// TODO: Change "0.39 / 1.0" to "0.39 / 2.0"
+#define RESISTOR				(0.39 / 1.0)
 
 //========== INTERRUPT ==========
 // INT0 : PD2
 // INT1 : PD3
 
 //========== ADC ================
-
-
 
 #define VOLTAGE_V5			(0)
 #define CURRENT_V5			(2)
--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 #include "lcd/hd44780.h"
 #include "jtag/jtag.h"
 
-char cahr_buffer[50];
+char cahr_buffer[200];
 uint8_t counter = 0;
 
 int main(void) {
@@ -38,19 +38,29 @@ int main(void) {
 		if (is_ready()) start_all_chanel_converrsion();
 		_delay_ms(10);
 
-		sprintf(cahr_buffer, "V: %u, A: %u", adc_value[VOLTAGE_V5] , adc_value[CURRENT_V5] );
+		sprintf(cahr_buffer, "C1: %f, C2: %f, C3: %f, C3: %f, C5: %f, C6: %f",
+				adc_value[VOLTAGE_V5] / 1023.0 * VREF,
+				adc_value[VOLTAGE_V12] / 1023.0 * VREF,
+				adc_value[CURRENT_V5] / 1023.0 * VREF,
+				adc_value[CURRENT_V12] / 1023.0 * VREF,
+				adc_value[MAX_CURRENT_V12] / 1023.0 * VREF,
+				adc_value[MAX_CURRENT_V5] / 1023.0 * VREF
+		);
 		send(cahr_buffer);
 
+		float volt_line_12 = adc_value[VOLTAGE_V12] / 1023.0 * VREF / SCALE_12V;
+		float volt_line_5 = adc_value[VOLTAGE_V5] / 1023.0 * VREF / SCALE_5V;
 
-		float volt_line_12 = adc_value[VOLTAGE_V12] / 1023.0 * VREF; // SCALE_12V;
-		float volt_line_5 = adc_value[VOLTAGE_V5] / 1023.0 * VREF; // SCALE_5V;
+		float amp_actual_line_12 = adc_value[CURRENT_V12] / 1023.0 * VREF / SCALE_OPAMP / RESISTOR;
+		float amp_actual_line_5 = adc_value[CURRENT_V5] / 1023.0 * VREF / SCALE_OPAMP / RESISTOR;
 
-
+		float amp_max_line_12 = adc_value[MAX_CURRENT_V12] / 1023.0 * VREF / SCALE_OPAMP / RESISTOR;
+		float amp_max_line_5 = adc_value[MAX_CURRENT_V5] / 1023.0 * VREF / SCALE_OPAMP / RESISTOR;
 
 		lcd_goto(0);
-		lcd_putsf(LCD_FORMAT, volt_line_5, 1.0, 2.0);
+		lcd_putsf(LCD_FORMAT, volt_line_5, amp_actual_line_5, amp_max_line_5);
 		lcd_goto(40);
-		lcd_putsf(LCD_FORMAT, volt_line_12, 1.0, 2.0);
+		lcd_putsf(LCD_FORMAT, volt_line_12, amp_actual_line_12, amp_max_line_12);
 	}
 
 }
